a29b9d8f30617ed254d5bc6fde995e9a7078cdad
Uche Igbokwe <uchehenryigbokwe@gmail.com>
2021-10-14 15:52:16 +0100

    implement publish method

--- Infra.Bus/RabbitMQBus.cs
+++ Infra.Bus/RabbitMQBus.cs
@@ -1,23 +1,47 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Domain.Core.Bus;
 using Domain.Core.Commands;
 using Domain.Core.Events;
+using MediatR;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 
 namespace Infra.Bus
 {
     public sealed class RabbitMQBus : IEventBus
     {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public RabbitMQBus(IMediator mediator)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+        }
         public void Publish<T>(T @event) where T : Event
         {
-            throw new NotImplementedException();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            var eventName = @event.GetType().Name;
+
+            channel.QueueDeclare(eventName, false, false, false, null);
+
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", eventName, null, body);
         }
 
         public Task SendCommand<T>(T command) where T : Command
         {
-            throw new NotImplementedException();
+            return _mediator.Send(command);
         }
 
         public void Subscribe<T, TH>()
